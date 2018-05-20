72a6f296b75e381ae769726264a088e2e93b2978
Ahmed Abdraboh <sleeplessmind919@gmail.com>
2018-05-20 17:52:32 +0200

    add build status image to readMe file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wAPIs
+
+[![Build Status](https://travis-ci.org/AhmedAbdraboh/wAPIs.svg?branch=master)](https://travis-ci.org/AhmedAbdraboh/wAPIs)
--- test/CarrierService.test.js
+++ test/CarrierService.test.js
@@ -4,6 +4,6 @@ const CarrierService = require('../src/services/CarrierService');
 
 describe('get the sum of two numbers', () => {
     it('should return 3', ()=>{
-        CarrierService.sum(1, 2).should.equal(4)
+        CarrierService.sum(1, 2).should.equal(3)
     })
 })
